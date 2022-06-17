d675884963c075e04ce963eafa724a1f1eaa542a
GitAccontPerso <didier.batlle@gmail.com>
2022-06-17 12:02:57 +0200

    Delete insert.sh

--- insert.sh
+++
@@ -1,6 +0,0 @@
-rm -f buffer
-mosquitto_pub -h localhost -t /$1/tty -m 'file.remove("buffer")'
-socat -d -d -d -U TCP4-LISTEN:3334,reuseaddr OPEN:$2 &
-
-echo "ecriture de $2 dans buffer"
-mosquitto_pub -h localhost -t /$1/sctw -m "buffer"
